022ec642257b6de0d1e452bff2fd2dc31847731a
po-panda <notcyanpascual@gmail.com>
2023-01-16 10:51:44 +0800

    feat: State machine

--- week-15/contracts/AdvanceERC721/CysAmazingPhotos.sol
+++ week-15/contracts/AdvanceERC721/CysAmazingPhotos.sol
@@ -6,20 +6,29 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract CysAmazingPhotos is Ownable, ERC721 {
+    using Counters for Counters.Counter;
+
     string constant NFT_NAME = "CysAmazingPhotos";
     string constant NFT_SYMBOL = "CyAP";
     uint256 private _mintPrice;
     string private baseURI;
-    using Counters for Counters.Counter;
     Counters.Counter private _numberOfNFTsMinted;
+    enum States{ PRESALE, PUBLIC, SOLDOUT }
+    States currentState;
+    
 
     constructor(uint256 mintPrice, string memory initialBaseUri) ERC721(NFT_NAME, NFT_SYMBOL){
         _mintPrice = mintPrice;
         baseURI = initialBaseUri;
     }
 
+    function _mintPresale() internal payable{
+        
+    }
+
     function mint() external payable {
         require(msg.value == _mintPrice, "CysAmazingPhotos: Not enough Ether.");
+        if (PRE)
         _numberOfNFTsMinted.increment();
         _mint(msg.sender, _numberOfNFTsMinted.current());
     }
